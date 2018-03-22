0727a53e673f712192ebcab5ee553bc2ac84a174
PedroBraz_Home <pmattos90@gmail.com>
2018-03-22 01:24:23 -0300

    Implemented PolePercentage with errors

--- DBManager.py
+++ DBManager.py
@@ -1,7 +1,9 @@
 import datetime as dt
+import numpy as np
 import pandas as pd
 import pymysql
 import time
+import re
 
 class DBManager():
 
@@ -112,7 +114,7 @@ class DBManager():
         """
 
         query = """
-            SELECT raceId, year, circuitId, name, date FROM F1.races ORDER BY date DESC
+            SELECT raceId, year, circuitId, name, date, round FROM F1.races ORDER BY date DESC
 
         """.format()
 
@@ -161,7 +163,8 @@ class DBManager():
 
     def getQualifying_table(self):
         """
-        Retrieves a pandas DataFrame with all results indexed by raceId-driver
+        Retrieves a pandas DataFrame with all results indexed by raceId-driver.
+        Also creates a column `qualiTime` with the driver' best qualifying lap time
         :return: pandas DF
         """
 
@@ -172,6 +175,18 @@ class DBManager():
         """.format()
 
         df = pd.read_sql(query, self.conn)
+        df.replace("None",np.nan,inplace=True)
+
+        df["q1"] = df["q1"].apply(self.adjust_second_colon)
+        df["q2"] = df["q2"].apply(self.adjust_second_colon)
+        df["q3"] = df["q3"].apply(self.adjust_second_colon)
+
+        df["q1"] = df["q1"].apply(self.createTimeDelta)
+        df["q2"] = df["q2"].apply(self.createTimeDelta)
+        df["q3"] = df["q3"].apply(self.createTimeDelta)
+
+        df["qualiTime"] = df[["q1", "q2", "q3", ]].min(axis=1) #.apply(dt.timedelta.total_seconds)
+
         df = df.set_index(["raceId","driverId"])
 
         return df.sort_index()
@@ -194,6 +209,65 @@ class DBManager():
 
         return df.set_index("driverId")
 
+
+    def getStandings_table(self):
+        """
+        Retrieves a pandas DataFrame with all standings info
+        :return: pandas DF
+        """
+
+        query = """
+
+            SELECT * FROM F1.driverStandings;
+
+        """.format()
+
+        df = pd.read_sql(query, self.conn)
+
+        df.set_index(["raceId","driverId"],inplace=True)
+
+
+        return df.sort_index()
+
+    def adjust_second_colon(self,s):
+        """
+        Aux function to convert strings like "1:23:453" to "1:23.453". This was necessary because we found
+        a few issues like this on the table `qualifying`
+        :return: string
+        """
+        #     return re.sub('(:[^:]*),', r'\1. ', string)
+
+        ret = None
+        if s is not None and s is not '':
+            # print(s)
+            ret = re.sub(r'^((?:(?!\:).)*\:(?:(?!\:).)*)\:', r'\1.', s)
+        return ret
+
+    def createTimeDelta(self,stri):
+        """
+        Aux function to create a time delta from a string with the format MM:SS.mili
+        Pandas can' do this
+
+        :return: TimeDelta
+        """
+        ret = None
+        if stri is not None and stri is not '':
+            fmt = "%M:%S.%f"
+            ret = dt.datetime.strptime(stri, fmt) - dt.datetime.strptime("00:00.000", fmt)
+        return ret
+
+    def total_seconds(self,td):
+        """
+        `TimeDelta`'s `total_seconds`method can't receive NaTType. This method can
+
+        :param td: TimeDelta or NaT
+        :return: float
+        """
+        ret = None
+
+        if td is not None:
+            ret = dt.timedelta.total_seconds(td)
+        return ret
 
 if __name__ == "__main__":
     rows = pd.read_pickle('test.pkl')
